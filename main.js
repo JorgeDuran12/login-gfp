ead3c802def291b79a571394a806c33f4cbece92
CodeSandbox <support@codesandbox.io>
2023-04-19 02:24:28 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const username = document.getElementById("email");
+const password = document.getElementById("password");
+const button = document.getElementById("button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+});
